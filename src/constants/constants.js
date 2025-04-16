9d6e03cb189edbef06316b406e5a516382f0de73
DiegoGonzalezBaeza <diego.gonzalez.baeza@gmail.com>
2025-04-15 22:08:05 -0400

    svg

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,7 @@
+import ideaImg from '/images/ideas.svg'
+import designsImg from '/images/designs.svg'
+import codeImg from '/images/code.svg'
+
 const navLinks = [
     {
       name: "Perfil",
@@ -18,9 +22,9 @@ const navLinks = [
   ];
 
 const words = [
-    { text: 'Ideas', imgPath: '/images/ideas.svg' },
-    { text: 'Creatividad', imgPath: "/images/ideas.svg" },
-    { text: 'Análisis', imgPath: '/images/ideas.svg' },
+    { text: 'Ideas', imgPath: ideaImg },
+    { text: 'Creatividad', imgPath: designsImg },
+    { text: 'Análisis', imgPath: codeImg },
     { text: 'Ideas', imgPath: '/images/ideas.svg' },
     { text: 'Creatividad', imgPath: "/images/ideas.svg" },
     { text: 'Análisis', imgPath: '/images/ideas.svg' },
